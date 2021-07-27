cfb70bda0027b888fa0d835debe95af9e13841f4
ddsjoberg <danield.sjoberg@gmail.com>
2021-07-27 10:21:12 -0400

    updates

--- R/add_tabset_codes.R
+++ R/add_tabset_codes.R
@@ -1,14 +1,14 @@
-#' Title
+#' Inserts Tabset Syntax by Language
 #'
 #' @param chunk_label chunk label stub
 #'
-#' @return
 #' @export
 add_tabset_codes <- function(chunk_label) {
   res <-
     knitr::knit_child(
       text = unlist(knitr::knit_expand('templates/tabset-template.Rmd')),
-      quiet = TRUE
+      quiet = TRUE,
+      envir = rlang::caller_env()
     )
   cat(res, sep = '\n')
 }
--- vignettes/rmd_chunks/stata-chunks.do
+++ vignettes/rmd_chunks/stata-chunks.do
@@ -1,3 +1,6 @@
 ## ---- stata-model -----
 * build logistic regression model
 logit am mpg
+
+## ---- stata-model2 -----
+logit am mpg
