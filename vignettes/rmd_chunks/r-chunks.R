03e252809a63adeb1da62c80d557f7ad4b8d1d5c
ddsjoberg <danield.sjoberg@gmail.com>
2021-07-27 13:54:23 -0400

    git2

---
+++ vignettes/rmd_chunks/r-chunks.R
@@ -0,0 +1,9 @@
+## ---- r-model -----
+# build logistic regression model
+mod <- glm(am ~ mpg, data = mtcars,family = binomial)
+# model summary
+gtsummary::tbl_regression(mod, intercept = TRUE)
+
+## ---- r-model2 -----
+# model summary
+gtsummary::tbl_regression(mod, intercept = TRUE)
