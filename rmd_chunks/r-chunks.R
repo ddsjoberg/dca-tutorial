54ebf988fa720b992c57e332f53d67c41c082fd7
Daniel D. Sjoberg <danield.sjoberg@gmail.com>
2021-08-03 15:37:26 -0400

    updates

--- rmd_chunks/r-chunks.R
+++ rmd_chunks/r-chunks.R
@@ -1,13 +1,96 @@
 ## ---- r-install -----
-# install dcurves from CRAN
-install.packages("dcurves")
+# install dcurves, tidyverse, gtsummary, and broom from CRAN
+install.packages(c("dcurves", "tidyverse", "gtsummary", "broom"))
 
 # load package
 library(dcurves)
+library(tidyverse)
+library(gtsummary)
+
+## ---- r-import_cancer -----
+# import data
+df_cancer_dx <-
+  readr::read_csv(
+    file = "https://raw.githubusercontent.com/ddsjoberg/dca-tutorial/main/data/df_cancer_dx.csv",
+    show_col_types = FALSE
+  )
+
+# summarize data
+df_cancer_dx %>%
+  select(-patientid) %>%
+  tbl_summary(type = all_dichotomous() ~ "categorical")
 
 ## ---- r-model -----
 # build logistic regression model
-mod <- glm(am ~ mpg, data = mtcars, family = binomial)
+mod1 <- glm(cancer ~ famhistory, data = df_cancer_dx, family = binomial)
 
 # model summary
-gtsummary::tbl_regression(mod, intercept = TRUE)
+mod1_summary <- tbl_regression(mod1, exponentiate = TRUE)
+mod1_summary
+
+## ---- r-dca_famhistory -----
+dca(cancer ~ famhistory, data = df_cancer_dx) %>%
+  plot()
+
+## ---- r-dca_famhistory2 -----
+dca(cancer ~ famhistory,
+    data = df_cancer_dx,
+    thresholds = seq(0, 0.35, 0.01)) %>%
+  plot()
+
+## ---- r-model_multi -----
+# build multivariable logistic regression model
+mod2 = glm(cancer ~ marker + age + famhistory, data = df_cancer_dx, family=binomial)
+
+# summarize model
+tbl_regression(mod2, exponentiate = TRUE)
+
+# add predicted values from model to data set
+df_cancer_dx <-
+  df_cancer_dx %>%
+  mutate(
+    cancerpredmarker =
+      broom::augment(mod2, type.predict = "response") %>%
+      pull(".fitted")
+  )
+
+## ---- r-dca_multi -----
+dca(cancer ~ famhistory + cancerpredmarker,
+    data = df_cancer_dx,
+    thresholds = seq(0, 0.35, 0.01)) %>%
+  plot(smooth = TRUE)
+
+## ---- r-pub_model -----
+# Use the coefficients from the Brown model
+df_cancer_dx <-
+  df_cancer_dx %>%
+  mutate(
+    logodds_brown = 0.75 * famhistory + 0.26 * age - 17.5,
+    phat_brown = exp(logodds_brown) / (1 + exp(logodds_brown))
+  )
+
+# Run the decision curve
+dca(cancer ~ phat_brown,
+    data = df_cancer_dx,
+    thresholds = seq(0, 0.35, 0.01)) %>%
+  plot(smooth = TRUE)
+
+## ---- r-joint -----
+#Create a variable for the strategy of treating only high risk patients
+df_cancer_dx <-
+  df_cancer_dx %>%
+  mutate(
+    #This will be 1 for treat and 0 for don’t treat
+    high_risk = ifelse(risk_group=="high", 1, 0),
+    #Treat based on Joint Approach
+    joint = ifelse(risk_group=="high" | cancerpredmarker > 0.15, 1, 0),
+    # Treat based on Conditional Approach
+    conditional =
+      ifelse(risk_group=="high" | (risk_group=="intermediate" & cancerpredmarker > 0.15), 1, 0)
+  )
+
+## ---- r-dca_joint -----
+dca(cancer ~ high_risk + joint + conditional,
+    data = df_cancer_dx,
+    thresholds = seq(0, 0.35, 0.01)) %>%
+  plot(smooth = TRUE)
