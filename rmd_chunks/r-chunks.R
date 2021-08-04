173c52cc79c8e54004858caf469e414b7b559cb6
ddsjoberg <danield.sjoberg@gmail.com>
2021-08-03 21:04:13 -0400

    updates

--- rmd_chunks/r-chunks.R
+++ rmd_chunks/r-chunks.R
@@ -1,6 +1,6 @@
 ## ---- r-install -----
 # install dcurves, tidyverse, gtsummary, and broom from CRAN
-install.packages(c("dcurves", "tidyverse", "gtsummary", "broom"))
+install.packages(c("dcurves", "tidyverse", "gtsummary", "broom", "survival"))
 
 # load package
 library(dcurves)
@@ -11,8 +11,7 @@ library(gtsummary)
 # import data
 df_cancer_dx <-
   readr::read_csv(
-    file = "https://raw.githubusercontent.com/ddsjoberg/dca-tutorial/main/data/df_cancer_dx.csv",
-    show_col_types = FALSE
+    file = "https://raw.githubusercontent.com/ddsjoberg/dca-tutorial/main/data/df_cancer_dx.csv"
   )
 
 # summarize data
@@ -80,9 +79,9 @@ dca(cancer ~ phat_brown,
 df_cancer_dx <-
   df_cancer_dx %>%
   mutate(
-    #This will be 1 for treat and 0 for don’t treat
+    # This will be 1 for treat and 0 for don’t treat
     high_risk = ifelse(risk_group=="high", 1, 0),
-    #Treat based on Joint Approach
+    # Treat based on Joint Approach
     joint = ifelse(risk_group=="high" | cancerpredmarker > 0.15, 1, 0),
     # Treat based on Conditional Approach
     conditional =
@@ -93,4 +92,72 @@ df_cancer_dx <-
 dca(cancer ~ high_risk + joint + conditional,
     data = df_cancer_dx,
     thresholds = seq(0, 0.35, 0.01)) %>%
+  plot(smooth = TRUE)
+
+## ---- r-dca_harm -----
+# the harm of measuring the marker is stored in a scalar
+harm_marker <- 0.0333
+# in the conditional test, only patients at intermediate risk
+# have their marker measured
+# harm of the conditional approach is proportion of patients who have the marker
+# measured multiplied by the harm
+harm_conditional <- mean(df_cancer_dx$risk_group == "intermediate") * harm_marker
+
+# Run the decision curve
+dca(cancer ~ high_risk + joint + conditional,
+    data = df_cancer_dx,
+    thresholds = seq(0, 0.35, 0.01),
+    harm = list(joint = harm_marker, conditional = harm_conditional)) %>%
+  plot(smooth = TRUE)
+
+
+## ---- r-dca_table -----
+dca(cancer ~ marker,
+    data = df_cancer_dx,
+    as_probability = "marker",
+    thresholds = seq(0.05, 0.35, 0.15)) %>%
+  as_tibble() %>%
+  select(label, threshold, net_benefit) %>%
+  gt::gt() %>%
+  gt::fmt_percent(columns = threshold, decimals = 0) %>%
+  gt::fmt(columns = net_benefit, fns = function(x) style_sigfig(x, digits = 3)) %>%
+  gt::cols_label(label = "Strategy",
+                 threshold = "Decision Threshold",
+                 net_benefit = "Net Benefit") %>%
+  gt::cols_align("left", columns = label)
+
+## ---- r-dca_intervention -----
+dca(cancer ~ marker,
+    data = df_cancer_dx,
+    as_probability = "marker",
+    thresholds = seq(0.05, 0.35, 0.01)) %>%
+  net_intervention_avoided() %>%
+  plot(smooth = TRUE)
+
+## ---- r-import_ttcancer -----
+# import data
+df_time_to_cancer_dx <-
+  readr::read_csv(
+    file = "https://raw.githubusercontent.com/ddsjoberg/dca-tutorial/main/data/df_time_to_cancer_dx.csv"
+  )
+
+## ---- r-coxph -----
+# Load survival library
+library(survival)
+
+# Run the cox model
+coxmod = coxph(Surv(ttcancer, cancer) ~ age + famhistory + marker, data = df_time_to_cancer_dx)
+
+df_time_to_cancer_dx <-
+  df_time_to_cancer_dx %>%
+  mutate(
+    pr_failure18 =
+      1 - summary(survfit(coxmod, newdata = df_time_to_cancer_dx), times=1.5)$surv[1,]
+  )
+
+## ---- r-stdca_coxph -----
+dca(Surv(ttcancer, cancer) ~ pr_failure18,
+    data = df_time_to_cancer_dx,
+    time = 1.5,
+    thresholds = seq(0, 0.5, 0.01)) %>%
   plot(smooth = TRUE)
