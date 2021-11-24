d97c99e78cba5039b48da7a8541ef2342cbe6df7
Daniel Sjoberg <danield.sjoberg@gmail.com>
2021-11-24 10:19:44 -0500

    saving scripts for each language

---
+++ rmd_chunks/chunks_as_scripts/python-script.py
@@ -0,0 +1,40 @@
+# ---- install ----- 
+import dcurves
+
+# ---- import_cancer ----- 
+
+# ---- model ----- 
+
+# ---- dca_famhistory ----- 
+
+# ---- dca_famhistory2 ----- 
+
+# ---- model_multi ----- 
+
+# ---- dca_multi ----- 
+
+# ---- pub_model ----- 
+
+# ---- joint ----- 
+
+# ---- dca_joint ----- 
+
+# ---- dca_harm ----- 
+
+# ---- dca_table ----- 
+
+# ---- dca_intervention ----- 
+
+# ---- import_ttcancer ----- 
+
+# ---- coxph ----- 
+
+# ---- stdca_coxph ----- 
+
+# ---- stdca_cmprsk ----- 
+
+# ---- import_case_control ----- 
+
+# ---- dca_case_control ----- 
+
+# ---- cross_validation ----- 
