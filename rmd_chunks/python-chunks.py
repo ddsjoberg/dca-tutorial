0c815825d5151e5e182d8123cf400be77cc333eb
Porwal <PorwalS@mskcc.org>
2022-12-30 14:08:39 -0500

    Updating python chunks

--- rmd_chunks/python-chunks.py
+++ rmd_chunks/python-chunks.py
@@ -1,19 +1,88 @@
 ## ---- python-install -----
 import dcurves
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+
 
 ## ---- python-import_cancer -----
 
+df_cancer_dx = pd.read_csv('https://raw.githubusercontent.com/ddsjoberg/dca-tutorial/main/data/df_cancer_dx.csv')
+
 ## ---- python-model -----
+
+mod1 = sm.GLM.from_formula('cancer ~ famhistory', data=df_cancer_dx, family=sm.families.Binomial())
+mod1_results = mod1.fit()
+
+print(mod1_results.summary())
 
 ## ---- python-dca_famhistory -----
 
+dca_result_df1 = \
+    dca(
+        data=df_cancer_dx,
+        outcome='cancer',
+        modelnames=['famhistory']
+    )
+
+plot_graphs(
+    plot_df=dca_result_df1
+)
+
 ## ---- python-dca_famhistory2 -----
+
+dca_result_df = \
+        dca(
+            data=df_cancer_dx,
+            outcome='cancer',
+            modelnames=['famhistory'],
+            thresholds=np.arange(0, 0.36, 0.01),
+        )
+
+plot_graphs(
+    plot_df=dca_result_df
+)
 
 ## ---- python-model_multi -----
 
+mod2 = sm.GLM.from_formula('cancer ~ marker + age + famhistory', data=df_cancer_dx, family=sm.families.Binomial())
+mod2_results = mod2.fit()
+
+print(mod2_results.summary())
+
 ## ---- python-dca_multi -----
 
+dca_result_df = \
+    dca(
+        data=df_cancer_dx,
+        outcome='cancer',
+        modelnames=['famhistory', 'cancerpredmarker'],
+        thresholds=np.arange(0,0.36,0.01)
+    )
+
+plot_graphs(
+    plot_df=dca_result_df,
+    y_limits=[-0.05, 0.2]
+    )
+
 ## ---- python-pub_model -----
+
+df_cancer_dx['logodds_brown'] = 0.75 * df_cancer_dx['famhistory'] + 0.26*df_cancer_dx['age'] - 17.5
+df_cancer_dx['phat_brown'] = np.exp(df_cancer_dx['logodds_brown']) / (1 + np.exp(df_cancer_dx['logodds_brown']))
+
+dca_result_df = \
+    dca(
+        data=df_cancer_dx,
+        outcome='cancer',
+        modelnames=['phat_brown'],
+        thresholds=np.arange(0,0.36,0.01),
+    )
+
+plot_graphs(
+    plot_df=dca_result_df,
+    y_limits=[-0.05, 0.2],
+    graph_type='net_benefit'
+)
 
 ## ---- python-joint -----
 
